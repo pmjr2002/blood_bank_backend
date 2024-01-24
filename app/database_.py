2a2d0d50c4ea2a58d7fd352b58feefb04d4b5a2f
jayanth_rao <jayanthrm.is21@rvce.edu.in>
2024-01-24 12:50:16 +0530

    Migrated from sqlite3 to postgresql

--- app/database_.py
+++ app/database_.py
@@ -2,16 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///../test.db"
+# **Update database URL for PostgreSQL**
+SQLALCHEMY_DATABASE_URL = "postgresql://avnadmin:AVNS_GxNqi95MDuVFYgFEyev@blood-db-wic-f.a.aivencloud.com:15185/defaultdb"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# **Create engine for PostgreSQL**
+engine = create_engine(SQLALCHEMY_DATABASE_URL)  # Remove "connect_args" for PostgreSQL
+
+# **Create session factory**
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# **Base for models remains unchanged**
 Base = declarative_base()
 
-# Dependency
+# **Dependency function remains unchanged**
 def get_db():
     db = SessionLocal()
     try:
