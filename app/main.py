d843dae68fa399b59cb1eddb276c13f353ed692f
= <pkb19032002@gmail.com>
2024-01-24 19:38:40 +0530

    Finally Fixed CORS Issueuvicorn main:app --reload!

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers import auth, hospitals, staff, requests, donors, donations, repository
 import models
 from database import engine
-from starlette.middleware import Middleware
+from fastapi.middleware import Middleware
 
 models.Base.metadata.create_all(bind=engine)
 
