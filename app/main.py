a201d3752936ac0cc428bb5d3c17a0462b6ef79f
= <pkb19032002@gmail.com>
2024-01-24 19:22:16 +0530

    Fixed CORS error

--- app/main.py
+++ app/main.py
@@ -4,18 +4,22 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers import auth, hospitals, staff, requests, donors, donations, repository
 import models
 from database import engine
+from starlette.middleware import Middleware
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+middleware = [
+    Middleware(
+        CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+    ]
+
+app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(hospitals.router)
