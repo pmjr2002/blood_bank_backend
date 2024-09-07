e75f3a36862125fa712babf8adae7ac6beff3eba
Sanjan-rao <usanjanrao@gmail.com>
2024-09-07 12:33:56 +0530

    fixed cors error

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ models.Base.metadata.create_all(bind=engine)
 middleware = [
     Middleware(
         CORSMiddleware,
-            allow_origins=["*"],
+            allow_origins=["http://localhost:3000", "https://blood-bank-front.onrender.com"],
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
